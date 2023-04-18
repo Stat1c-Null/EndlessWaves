4a42275debea06f9322601f881ee2d53bd0d95a1
Mikita Slabysh <slabysh2015@gmail.com>
2023-04-18 12:45:53 -0400

    Started working on proper third person camera

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
